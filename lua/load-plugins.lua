5fc26086f3080c7e340d45b3dd59ce3031a1f7bb
Elson Wu <weijwu@expediagroup.com>
2024-06-17 22:08:17 +0800

    chore: simplify the lazy setup

--- lua/load-plugins.lua
+++ lua/load-plugins.lua
@@ -7,50 +7,34 @@ require('lazy').setup({
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        lazy = true,
-        event = "UIEnter",
         config = function() require('setup-plugins.treesitter') end
     }, {
         "nvim-treesitter/nvim-treesitter-textobjects",
-        lazy = true,
-        event = "UIEnter",
         dependencies = {"nvim-treesitter/nvim-treesitter"}
     }, -- Telescope
     {
         'nvim-telescope/telescope.nvim',
-        lazy = true,
-        event = "UIEnter",
         dependencies = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim'},
         config = function() require('setup-plugins.telescope') end
     }, -- Telescope plugins
     {
         'nvim-telescope/telescope-fzf-native.nvim',
-        lazy = true,
-        event = "UIEnter",
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
         dependencies = {'nvim-telescope/telescope.nvim'}
     }, -- File exporer
     {
         'nvim-tree/nvim-tree.lua',
         dependencies = {'nvim-tree/nvim-web-devicons'},
-        priority = 100,
-        -- lazy = true,
-        -- event = "UIEnter",
-        -- version = 'nightly',
         config = function() require('setup-plugins.nvim-tree') end
     }, {
         'nvim-lualine/lualine.nvim',
-        lazy = true,
-        event = "UIEnter",
-        dependencies = {'nvim-tree/nvim-web-devicons'},
+        dependencies = {'nvim-tree/nvim-web-devicons', opt=true},
         config = function() require('setup-plugins.lualine') end
-    }, 
+    },
     {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-    {'lewis6991/gitsigns.nvim', lazy = true, event = 'UIEnter', config = true},
+    {'lewis6991/gitsigns.nvim', config = true},
     {
         'akinsho/git-conflict.nvim',
-        lazy = true,
-        event = "UIEnter",
         version = '*',
         opt = {
             {
@@ -61,33 +45,23 @@ require('lazy').setup({
         },
         config = true
     }, -- utils
-    {'Raimondi/delimitMate', lazy = true, event = "UIEnter"},
-    {'tpope/vim-commentary', lazy = true, event = "UIEnter"},
-    {'djoshea/vim-autoread', lazy = true, event = "UIEnter"}, {
-        'gelguy/wilder.nvim',
-        lazy = true,
-        event = "UIEnter",
-        config = function() require('setup-plugins.wilder') end
-    }, {'jparise/vim-graphql', lazy = true, event = "UIEnter"},
-
-    {'qpkorr/vim-bufkill', lazy = true, event = "UIEnter"}, {
-        'ggandor/leap.nvim',
-        lazy = true,
-        event = "UIEnter",
-        config = function() require('setup-plugins.leap') end
-    }, {'tpope/vim-repeat', lazy = true, event = "UIEnter"},
-    {'tpope/vim-surround', lazy = true, event = "UIEnter"},
-    {'tpope/vim-sleuth', lazy = true, event = "UIEnter"},
-    {'sbdchd/neoformat', lazy = true, event = "UIEnter"},
-    {'Pocco81/HighStr.nvim', lazy = true, config = true, event = "UIEnter"}, {
-        'iamcco/markdown-preview.nvim',
-        lazy = true,
-        event = "UIEnter",
-        build = function() vim.fn['mkdp#util#install']() end
-    }, -- search & replace
-    {'windwp/nvim-spectre', lazy = true, event = "UIEnter"},
-    {'windwp/nvim-autopairs', lazy = true, event = "UIEnter", config = true},
-    {'jxnblk/vim-mdx-js', lazy = true}, {
+    {'Raimondi/delimitMate'},
+    {'tpope/vim-commentary'},
+    {'djoshea/vim-autoread'},
+    {'gelguy/wilder.nvim', config = function() require('setup-plugins.wilder') end },
+    {'jparise/vim-graphql'},
+    {'qpkorr/vim-bufkill'},
+    {'ggandor/leap.nvim', config = function() require('setup-plugins.leap') end },
+    {'tpope/vim-repeat'},
+    {'tpope/vim-surround'},
+    {'tpope/vim-sleuth'},
+    {'sbdchd/neoformat'},
+    {'Pocco81/HighStr.nvim'},
+    {'iamcco/markdown-preview.nvim', build = function() vim.fn['mkdp#util#install']() end },
+    -- search & replace
+    {'windwp/nvim-spectre'},
+    {'windwp/nvim-autopairs', config = true},
+    {'jxnblk/vim-mdx-js'}, {
         'goolord/alpha-nvim',
         dependencies = {'nvim-tree/nvim-web-devicons'},
         config = function() require('setup-plugins.alpha') end
@@ -107,35 +81,18 @@ require('lazy').setup({
             'hrsh7th/cmp-nvim-lsp', -- lsp completions
             'hrsh7th/cmp-path' -- file path completions
         },
-        lazy = true,
-        event = "UIEnter",
         config = function() require('setup-plugins.cmp') end
     }, -- Better rust support
-    {
-        'simrat39/rust-tools.nvim',
-        lazy = true,
-        dependencies = {'neovim/nvim-lspconfig'}
-    }, -- LSP Support
-    {'neovim/nvim-lspconfig', lazy = true, event = "UIEnter"}, {
-        'williamboman/mason.nvim',
-        lazy = true,
-        event = "UIEnter",
-        build = ':MasonUpdate',
-        config = function() require('setup-plugins.mason') end
-    }, {
-        'williamboman/mason-lspconfig.nvim',
-        lazy = true,
-        event = "UIEnter",
-        dependencies = {'williamboman/mason.nvim'},
-        config = function() require('setup-plugins.mason-lspconfig') end
-    }, {'b0o/schemastore.nvim', lazy = true, event = "UIEnter"}, {
-        'glepnir/lspsaga.nvim',
+    {'simrat39/rust-tools.nvim', dependencies = {'neovim/nvim-lspconfig'}}, -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim', build = ':MasonUpdate', config = function() require('setup-plugins.mason') end },
+    {'williamboman/mason-lspconfig.nvim', dependencies = {'williamboman/mason.nvim'}, config = function() require('setup-plugins.mason-lspconfig') end },
+    {'b0o/schemastore.nvim'},
+    {'glepnir/lspsaga.nvim',
         branch = 'main',
         dependencies = {
             {'williamboman/mason-lspconfig.nvim', 'neovim/nvim-lspconfig'}
         },
-        lazy = true,
-        event = 'UIEnter',
         config = function()
             require("lspsaga").setup({request_timeout = 5000})
         end
