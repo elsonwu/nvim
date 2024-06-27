f3b92ae34b3a926c721c090652a26d0a49ef386d
Elson Wu <weijwu@expediagroup.com>
2024-06-27 21:30:09 +0800

    chore: re-format and refine some key for telescope

--- lua/setup-plugins/telescope.lua
+++ lua/setup-plugins/telescope.lua
@@ -1,21 +1,7 @@
 local actions = require("telescope.actions")
 
 require("telescope").setup({
-	file_ignore_patterns = { "node_modules", ".git" },
-	-- pickers = {
-	--     live_grep = {additional_args = function(opts) return {"--hidden"} end}
-	-- },
-	-- defaults = {
-	--     initial_mode = 'insert',
-	--     path_display = {'absolute'},
-	--     wrap_results = true,
-	--     layout_strategy = 'vertical',
-	--     layout_config = {height = 0.95, width = 0.95},
-	--     default_mappings = false,
-	--     mappings = {
-	--         i = {["<C-j>"] = require('telescope.actions').move_selection_next}
-	--     }
-	-- },
+	-- file_ignore_patterns = { "node_modules", ".git" },
 	defaults = {
 		layout_config = { height = 0.95, width = 0.95 },
 		layout_strategy = "vertical",
@@ -28,6 +14,7 @@ require("telescope").setup({
 			"--column",
 			"--smart-case",
 		},
+		entry_prefix = "  ",
 		prompt_prefix = "> ",
 		selection_caret = "> ",
 		path_display = { "smart" },
@@ -39,33 +26,32 @@ require("telescope").setup({
 				["<CR>"] = actions.select_default,
 			},
 		},
-		file_ignore_patterns = { "node_modules", "%.git/", "%.cache/" },
 	},
-	pickers = {
-		find_files = {
-			theme = "dropdown",
-		},
-		git_files = {
-			theme = "dropdown",
-		},
-		live_grep = {
-			theme = "dropdown",
-		},
-		buffers = {
-			theme = "dropdown",
-		},
-		help_tags = {
-			theme = "dropdown",
-		},
-	},
-	extensions = {
-		fzf = {
-			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = true, -- override the generic sorter
-			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-		},
-	},
+	-- pickers = {
+	-- 	find_files = {
+	-- 		theme = "dropdown",
+	-- 	},
+	-- 	git_files = {
+	-- 		theme = "dropdown",
+	-- 	},
+	-- 	live_grep = {
+	-- 		theme = "dropdown",
+	-- 	},
+	-- 	buffers = {
+	-- 		theme = "dropdown",
+	-- 	},
+	-- 	help_tags = {
+	-- 		theme = "dropdown",
+	-- 	},
+	-- },
+	-- extensions = {
+	-- 	fzf = {
+	-- 		fuzzy = true, -- false will only do exact matching
+	-- 		override_generic_sorter = true, -- override the generic sorter
+	-- 		override_file_sorter = true, -- override the file sorter
+	-- 		case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+	-- 	},
+	-- },
 	-- defaults = {
 	--     initial_mode = 'insert',
 	--     path_display = {'absolute'},
@@ -100,22 +86,32 @@ end
 local keymap = vim.keymap.set
 -- search + LSP
 local builtin = require("telescope.builtin")
--- keymap('n', '<leader>sn', ':Telescope node_modules list<CR>')
-keymap("n", "<leader>sf", ":Telescope find_files find_command=rg,--smart-case,--ignore,--hidden,--files<CR>")
-keymap("n", "<leader>ss", builtin.live_grep, {})
-keymap("n", "<leader>sw", builtin.grep_string, {})
-keymap("n", "<leader>sb", builtin.buffers, {})
+keymap("n", "<leader>sn", ":Telescope node_modules list<CR>", { noremap = true, silent = true })
+keymap(
+	"n",
+	"<leader>sf",
+	":Telescope find_files find_command=rg,--smart-case,--ignore,--hidden,--files<CR>",
+	{ noremap = true, silent = true }
+)
+keymap("n", "<leader>ss", builtin.live_grep, { noremap = true, silent = true })
+keymap(
+	"n",
+	"<leader>sw",
+	':lua require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") })<CR>',
+	{ noremap = true, silent = true }
+)
+keymap("n", "<leader>sb", builtin.buffers, { noremap = true, silent = true })
 -- keymap('n', '<leader>sm', builtin.marks, {})
 -- search and replace current word
 keymap("n", "<leader>sr", ":lua require('spectre').open_visual({select_word=true})<CR>", {})
 
-keymap("n", "gd", builtin.lsp_definitions, { silent = true })
-keymap("n", "<leader>gk", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
-keymap("n", "<leader>gt", builtin.lsp_type_definitions, { silent = true })
-keymap("n", "<leader>gr", "<cmd>Lspsaga finder<CR>", { silent = true })
-keymap("n", "<leader>gi", builtin.lsp_implementations, { silent = true })
-keymap("n", "<leader>gE", "<cmd>Lspsaga show_buf_diagnostics<CR>", { silent = true })
-keymap("n", "<leader>ge", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+keymap("n", "gd", builtin.lsp_definitions, { noremap = true, silent = true })
+keymap("n", "<leader>gk", "<cmd>Lspsaga peek_definition<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>gt", builtin.lsp_type_definitions, { noremap = true, silent = true })
+keymap("n", "<leader>gr", "<cmd>Lspsaga finder<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>gi", builtin.lsp_implementations, { noremap = true, silent = true })
+keymap("n", "<leader>gE", "<cmd>Lspsaga show_buf_diagnostics<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>ge", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { noremap = true, silent = true })
 
 -- Map the function to a command or keybinding
 keymap("n", "<leader>sm", "<cmd>lua SearchNodeModules()<CR>", { noremap = true, silent = true })
